77b540962c81455d9fd77ae2916ada176287adf7
楠z✰HisakoR <84510571+HisakoR@users.noreply.github.com>
2024-10-11 18:44:46 +0800

    Update Main.java

--- Kangrds/src/Main.java
+++ Kangrds/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        mainMenu menuMain = new mainMenu();
-        menuMain.menu();
+        loginMenu menuMain = new loginMenu();
+        menuMain.login();
     }
 }
