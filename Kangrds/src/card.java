873e6187ec3cb7ad5befc67d49085e11fac20ad8
楠z✰HisakoR <84510571+HisakoR@users.noreply.github.com>
2024-10-11 11:46:27 +0800

    Add files via upload

---
+++ Kangrds/src/card.java
@@ -0,0 +1,18 @@
+public class card {
+    private int type;
+    /**
+     * 0总部
+     * 1指令
+     * 2单位
+     */
+    private String name;
+    private int atk;
+    private int def;
+    private int cost;
+    private int move;
+    //效果
+    public card() {
+
+    }
+
+}
---
+++ Kangrds/src/mainMenu.java
@@ -0,0 +1,13 @@
+public class mainMenu {
+    private boolean isOn = true;
+    public void menu(){
+        while(isOn){
+            System.out.println("=====================================");
+            System.out.println("=                kangrds            =");
+            System.out.println("=====================================");
+            System.out.println("=[1]start                           =");
+            System.out.println("=[2]setting                         =");
+            System.out.println("=====================================");
+        }
+    }
+}
