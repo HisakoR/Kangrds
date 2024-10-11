90d9e089f7d4528a6854aa7cd45a0056dae1048e
楠z✰HisakoR <84510571+HisakoR@users.noreply.github.com>
2024-10-11 18:52:09 +0800

    Update loginMenu.java

--- Kangrds/src/loginMenu.java
+++ Kangrds/src/loginMenu.java
@@ -3,22 +3,21 @@ import java.util.Scanner;
 public class loginMenu{
     private String userName;
     private String password;
-    private boolean isOn = true;
     public static Scanner scan = new Scanner(System.in);
-    private String input;
 
     public void login(){
         mainMenu main = new mainMenu();
-        while(isOn){
-            if (!main.isExit()){
-                break;
-            }
-            System.out.println("Please input your user name:");
-            userName = loginMenu.scan.nextLine();
-            System.out.println("Please input your password:");
-            password = loginMenu.scan.nextLine();
-            System.out.println("Welcome to Kangrds!");
-            main.menu();
-        }
+        System.out.println("Please input your user name:");
+        userName = loginMenu.scan.nextLine();
+        System.out.println("Please input your password:");
+        password = loginMenu.scan.nextLine();
+        System.out.println("Welcome to Kangrds!");
+        main.menu();
+    }
+    public String getUserName(){
+        return userName;
+    }
+    public String getPassword(){
+        return password;
     }
 }
