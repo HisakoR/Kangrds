01bd54beefe7c5c5b738b4113937499f64a19649
楠z✰HisakoR <84510571+HisakoR@users.noreply.github.com>
2024-10-14 20:39:25 +0800

    Add files via upload

---
+++ Kangrds/src/battleField.java
@@ -0,0 +1,8 @@
+public class battleField {
+    private card[][] field = new card[5][3];
+    private card[] playerHandle = new card[8];
+    private deck playerDeck;
+    public void intro(){
+
+    }
+}
---
+++ Kangrds/src/event.java
@@ -0,0 +1,81 @@
+public class event {
+
+    //强制事件组
+    public void semesterStart(){
+        //学期开始的事件
+    }
+    public void semesterEnd(){
+        //学期结束的事件
+    }
+    public void midterm(){
+        //期中事件
+    }
+
+    //随机大型事件组
+    public void typhoon(){
+        //台风事件
+    }
+    public void heavyRain(){
+        //大暴雨事件
+    }
+    public void fire(){
+        //火灾事件
+    }
+    public void explode(){
+        //爆炸事件
+    }
+    public void earthquake(){
+        //地震事件
+    }
+    public void plague(){
+        //流感事件
+    }
+
+    //随机专业事件
+    public void concert(){
+        //音乐厅事件
+    }
+    public void match(){
+        //赛事事件(篮球，足球，飞盘)
+    }
+    public void show(){
+        //艺术展事件
+    }
+    public void vocabTest(){
+        //单词测事件
+    }
+    public void langTest(){
+        //标化考试事件
+    }
+
+    //fun值事件
+    public void funA(){
+        //恋爱事件 | 进度1
+    }
+    public void funB(){
+        //恋爱事件 | 进度2
+    }
+    public void funC(){
+        //恋爱事件 | 进度3
+    }
+    public void funD(){
+        //恋爱事件 | 进度4
+    }
+    public void funE(){
+        //恋爱事件 | 进度5
+    }
+    public void funF(){
+        //恋爱事件 | 失败
+    }
+    public void funG(){
+        //恋爱事件 | 成功
+    }
+
+    //小型事件组
+    public void psDeath(){
+        //校长の似事件
+    }
+    public void greatPurge(){
+        //大清洗事件(随机清除老师)
+    }
+}
