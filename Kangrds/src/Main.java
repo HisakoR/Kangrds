06d49cff18de04bbb94b94fd106f44dc5e83590f
楠z✰HisakoR <84510571+HisakoR@users.noreply.github.com>
2024-10-15 10:29:26 +0800

    main

--- Kangrds/src/Main.java
+++ Kangrds/src/Main.java
@@ -1,7 +1,23 @@
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
-    public static void main(String[] args) {
-        loginMenu menuMain = new loginMenu();
-        menuMain.login();
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        //初始化程序
+        Pane root = new Pane();//新操作面板
+        Scene scene = new Scene(root, 1280, 720);//大小
+        primaryStage.setResizable(false);//不可调节大小
+        //界面内容
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
+    //主要方法
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }
--- Kangrds/src/battleField.java
+++ Kangrds/src/battleField.java
@@ -1,25 +1,6 @@
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.layout.*;
-import javafx.stage.Stage;
-
-public class battleField extends Application {
+public class battleField {
     private card[][] field = new card[5][3];
     private card[] playerHandle = new card[8];
     private deck playerDeck;
 
-    @Override
-    public void start(Stage primaryStage) {
-        //初始化程序
-        Pane root = new Pane();//新操作面板
-        Scene scene = new Scene(root, 500, 800);//大小
-        primaryStage.setResizable(false);//不可调节大小
-        //界面内容
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-    //主要方法
-    public static void main(String[] args) {
-        launch(args);
-    }
 }
